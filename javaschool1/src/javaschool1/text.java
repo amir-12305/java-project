f139e6e816c73703b4fad870e609fc409c8c2957
amirkhan <amirkhan@DESKTOP-6OLPDOG>
2020-11-07 12:44:19 +0530

    first commit

--- javaschool1/src/javaschool1/text.java
+++ javaschool1/src/javaschool1/text.java
@@ -1,10 +1,15 @@
 package javaschool1;
-
+import java.util.Scanner;
 public class text {
+   public static void main(String[] args) {
+		 Scanner s = new Scanner(System.in);
+			System.out.print("Enter number:");        
+			int n=s.nextInt();
+		        for(int i=1; i <= 10; i++)
+		        {
+		            System.out.println(n+" * "+i+" = "+n*i);
+		        }
+		    }
+		}	
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
-
-}
